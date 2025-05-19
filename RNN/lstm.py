30f36eaff9c303f4034a6a625d01a526ce9543f6
zaineb <103284201+zcybrgd@users.noreply.github.com>
2025-05-19 11:17:23 +0100

    simple lstm

---
+++ RNN/lstm.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])  # last time step
+        return out
