f9557eda41df42fd5f7afd6aea0ec2755792dcd3
zaineb <103284201+zcybrgd@users.noreply.github.com>
2025-05-19 11:16:24 +0100

    simple rnn model

---
+++ RNN/rnn.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class VanillaRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(VanillaRNN, self).__init__()
+        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out, _ = self.rnn(x)
+        out = self.fc(out[:, -1, :])
+        return out
+
+
+
+'''
+ht​=tanh(Wh*​h(t−1)​+Wx*​xt​+b)
+y^t=softmax(Wy*ht+by)
+'''
